9204bd398da95f44510896dde25fe544d8d04e3c
Anthony <anthony.eccleston@live.com>
2024-06-28 10:05:27 -0400

    ,All  done

--- server.js
+++ server.js
@@ -6,17 +6,15 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 const authController = require('./controllers/auth.js');
-
+const applicationsController = require('./controllers/applications.js');
 const port = process.env.PORT ? process.env.PORT : '3000';
-
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
-
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
@@ -27,15 +25,20 @@ app.use(
     saveUninitialized: true,
   })
 );
-
+app.use(passUserToView); // add here
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
+  // Check if the user is logged in
+  if (req.session.user) {
+    // Redirect logged-in users to their applications index
+    res.redirect(`/users/${req.session.user._id}/applications`);
+  } else {
+    // Show the homepage for users who are not logged in
+    res.render('index.ejs');
+  }
 });
-
 app.use('/auth', authController);
-
+app.use(isSignedIn);
+app.use('/users/:userId/applications', applicationsController); // updated
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
 });
